b47dc7fd8f1c773d8b5fdd3f6cb8fe23a2f854df
billaue2 <bill.aue@gmail.com>
2016-02-26 13:25:48 +0700

    Add cryptonight-light (cryptolight) support

--- multihashing.cc
+++ multihashing.cc
@@ -18,11 +18,12 @@ extern "C" {
     #include "hefty1.h"
     #include "shavite3.h"
     #include "cryptonight.h"
+    #include "cryptolight.h"
     #include "x13.h"
     #include "nist5.h"
     #include "sha1.h",
     #include "x15.h"
-	#include "fresh.h"
+    #include "fresh.h"
 }
 
 #include "boolberry.h"
@@ -429,6 +430,39 @@ Handle<Value> cryptonight(const Arguments& args) {
     return scope.Close(buff->handle_);
 }
 
+Handle<Value> cryptolight(const Arguments& args) {
+    HandleScope scope;
+
+    bool fast = false;
+
+    if (args.Length() < 1)
+        return except("You must provide one argument.");
+    
+    if (args.Length() >= 2) {
+        if(!args[1]->IsBoolean())
+            return except("Argument 2 should be a boolean");
+        fast = args[1]->ToBoolean()->BooleanValue();
+    }
+
+    Local<Object> target = args[0]->ToObject();
+
+    if(!Buffer::HasInstance(target))
+        return except("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char output[32];
+    
+    uint32_t input_len = Buffer::Length(target);
+
+    if(fast)
+        cryptolight_fast_hash(input, output, input_len);
+    else
+        cryptolight_hash(input, output, input_len);
+
+    Buffer* buff = Buffer::New(output, 32);
+    return scope.Close(buff->handle_);
+}
+
 Handle<Value> x13(const Arguments& args) {
     HandleScope scope;
 
@@ -591,6 +625,7 @@ void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("hefty1"), FunctionTemplate::New(hefty1)->GetFunction());
     exports->Set(String::NewSymbol("shavite3"), FunctionTemplate::New(shavite3)->GetFunction());
     exports->Set(String::NewSymbol("cryptonight"), FunctionTemplate::New(cryptonight)->GetFunction());
+    exports->Set(String::NewSymbol("cryptolight"), FunctionTemplate::New(cryptolight)->GetFunction());
     exports->Set(String::NewSymbol("x13"), FunctionTemplate::New(x13)->GetFunction());
     exports->Set(String::NewSymbol("boolberry"), FunctionTemplate::New(boolberry)->GetFunction());
     exports->Set(String::NewSymbol("nist5"), FunctionTemplate::New(nist5)->GetFunction());
