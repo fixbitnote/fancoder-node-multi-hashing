2a4474da9e597de56bea34b1ebb596fda1e314e8
SondysIT <37102047+SondysIT@users.noreply.github.com>
2018-03-09 07:10:39 +0100

    Update cryptolight.c

--- cryptolight.c
+++ cryptolight.c
@@ -12,7 +12,7 @@
 #include "crypto/hash-ops.h"
 
 #define MEMORY (1 << 19)
-#define ITER   (1 << 19)
+#define ITER   (1 << 20)
 #define AES_BLOCK_SIZE  16
 #define AES_KEY_SIZE    32
 #define INIT_SIZE_BLK   8
